440225e4de6b8e0e85ae0590e1b61aa7e78f9a02
Andrew Clarke <andrewclarke982@gmail.com>
2017-06-26 16:39:25 +0100

    installed clearance gem for authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  constraints Clearance::Constraints::SignedIn.new do
+    root to: "dashboards#show", as: :signed_in_root
+  end
+
+  constraints Clearance::Constraints::SignedOut.new do
+    root to: "marketing#index"
+  end
+
   resources :posts do
     resources :comments
   end
