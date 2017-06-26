48132ac1a7064b1d1a898e3eef48e3d863c4cd67
Michael Jacobson <michael.jacobson89@gmail.com>
2017-06-26 17:44:38 +0100

    • add functionality for users to sign-up, sign-in and sign-out

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
   include Clearance::User
+
+  # attr_accessor :username
+  #
+  # validates_presence_of :username
+  # validates_uniqueness_of :username
+  #
+  # def self.authenticate(email, password)
+  #   user = find(:first, :conditions => ['username = ? OR email = ?', email.to_s.downcase, email.to_s.downcase])
+  #   user && user.authenticated?(password) ? user : nil
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   constraints Clearance::Constraints::SignedIn.new do
-    root to: "dashboards#show", as: :signed_in_root
+    resources :posts
   end
 
-  constraints Clearance::Constraints::SignedOut.new do
-    root to: "marketing#index"
-  end
+  # constraints Clearance::Constraints::SignedOut.new do
+  #   root to: "sign_in"
+  # end
 
   resources :posts do
     resources :comments
