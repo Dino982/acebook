a79a9e1ce3d7af090a8e67a1f1ce4ec0c1cd5c36
Charlotte Fereday <cbrfereday@gmail.com>
2017-06-26 14:54:17 +0100

    User can add comments to a post -Updates comments model -Updates comments controller -Updates comments feature & unit spec

--- spec/features/user_can_add_comments_to_post_spec.rb
+++ spec/features/user_can_add_comments_to_post_spec.rb
@@ -3,9 +3,11 @@ require 'rails_helper'
 RSpec.feature "Timeline", type: :feature do
   scenario "Can add comments to posts and view them" do
     visit "/posts"
-    click_link "Comment"
-    fill_in "Comment", with: "Amazing post!!!"
+    click_link "New post"
+    fill_in "Message", with: "Test post"
     click_button "Submit"
+    fill_in "Body", with: "Amazing post!!!"
+    click_button "Create Comment"
     expect(page).to have_content("Amazing post!!!")
   end
 end
