e73d0177f004c6fe26360ecc7ecfafcf2463db96
Panteha <panteha.ahmadi@gmail.com>
2017-06-27 22:53:54 +0100

    Connected sign_in route to post route

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   constraints Clearance::Constraints::SignedIn.new do
-    resources :posts
+    resources :posts do
+      resources :comments
+    end
   end
 
-  # constraints Clearance::Constraints::SignedOut.new do
-  #   root to: "sign_in"
-  # end
-
-  resources :posts do
-    resources :comments
+  constraints Clearance::Constraints::SignedOut.new do
+    get '/posts', to: redirect('/sign_in')
   end
+
+
 end
