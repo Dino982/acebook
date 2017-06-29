754292588a91c72723791185cdf9ca6d3f25e56e
Panteha Ahmadi <panteha.ahmadi@gmail.com>
2017-06-29 16:04:35 +0100

    Update likes_controller.rb

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,7 +3,6 @@ class LikesController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @like = @post.likes.create(like_params)
-    # @likes_count = Like.where(post_id: @post.id).ids.length
     redirect_to posts_url
   end
 
