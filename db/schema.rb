99d9593baef238a69f292ca34819e62dc732901f
Alex <alex.satur89@gmail.com>
2017-06-30 11:30:27 +0100

    Adding photos to user posts WIP

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170629125202) do
+ActiveRecord::Schema.define(version: 20170630102112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,10 @@ ActiveRecord::Schema.define(version: 20170629125202) do
     t.text "username"
     t.string "firstname"
     t.string "lastname"
+    t.string "avatar_file_name"
+    t.string "avatar_content_type"
+    t.integer "avatar_file_size"
+    t.datetime "avatar_updated_at"
     t.index ["email"], name: "index_users_on_email"
     t.index ["remember_token"], name: "index_users_on_remember_token"
   end
